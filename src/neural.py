f568b9284ed33d135b8506ced69f32ecfc3a5c73
Shing Chan <cshing.m@gmail.com>
2021-03-14 22:34:05 +0000

    cohort2020 update

--- src/neural.py
+++ src/neural.py
@@ -1,22 +1,19 @@
-# %%
+# %% [markdown]
 '''
-# Activity recognition on the Capture24 dataset
+# Neural networks in activity recognition
 
-## Neural networks for feature engineering
+Engineering effective features is one of the most laborious parts of machine
+learning. 
+The appeal of modern neural networks is that feature enginnering is 
+integrated into the training process &mdash; they automatically engineer
+features that are relevant for the learning task, directly from the raw
+representation of the data
 
-*This section assumes familiarity with [PyTorch](https://pytorch.org/)*
-
-Crafting effective features is one of the most crucial parts of
-machine learning. It is also one of the most difficult and time consuming. So far we
-trained a random forest using features borrowed from various sources in the
-literature.
-In this section, we look at using a neural network to automatically craft
-relevant features from the raw accelerometer data for the learning task.
-
-###### Setup
+## Setup
 '''
 
 # %%
+import json
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
@@ -24,6 +21,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import torch.backends.cudnn as cudnn
+from sklearn import metrics
 from tqdm.auto import tqdm
 import utils
 
@@ -32,10 +30,7 @@ np.random.seed(42)
 torch.manual_seed(42)
 cudnn.benchmark = True
 
-# %%
-''' ###### Grab a GPU if there is one '''
-
-# %%
+# Grab a GPU if there is one
 if torch.cuda.is_available():
     device = torch.device("cuda")
     print("Using {} device: {}".format(device, torch.cuda.current_device()))
@@ -43,44 +38,65 @@ else:
     device = torch.device("cpu")
     print("Using {}".format(device))
 
-# %%
-''' ###### Load dataset and hold out some instances for testing '''
-
-# %%
-data = np.load('capture24.npz', allow_pickle=True)
-# data = np.load('capture24_small.npz', allow_pickle=True)
-print("Contents of capture24.npz:", data.files)
-y, pid, time = data['y'], data['pid'], data['time']
-X = np.load('X_raw.npy', mmap_mode='r')
-# X = np.load('X_raw_small.npy')
-print("Raw dataset shape:", X.shape)
-
-# Hold out some participants for testing the model
-pids_test = [2, 3]
-mask_test = np.isin(pid, pids_test)
-mask_train = ~mask_test
-y_train, y_test = y[mask_train], y[mask_test]
-pid_train, pid_test = pid[mask_train], pid[mask_test]
-# X[mask_train] and X[mask_test] if you like to live dangerously
-X_train = utils.ArrayFromMask(X, mask_train)
-X_test = utils.ArrayFromMask(X, mask_test)
-print("Shape of X_train", X_train.shape)
-print("Shape of X_test", X_test.shape)
-
-# %%
+# %% [markdown]
+''' 
+## Load dataset 
 '''
-###### Architecture design
 
-As a baseline model, we use a convolutional neural network (CNN) with a
+# %%
+
+DATASET_PATH = 'dataset/'  # path to your extracted windows
+
+with open(DATASET_PATH+'info.json', 'r') as f:
+    info = json.load(f)
+
+X = np.memmap(DATASET_PATH+'X.dat', mode='r', dtype=info['X_dtype'], shape=tuple(info['X_shape']))
+Y = np.memmap(DATASET_PATH+'Y.dat', mode='r', dtype=info['Y_dtype'], shape=tuple(info['Y_shape']))
+T = np.memmap(DATASET_PATH+'T.dat', mode='r', dtype=info['T_dtype'], shape=tuple(info['T_shape']))
+P = np.memmap(DATASET_PATH+'P.dat', mode='r', dtype=info['P_dtype'], shape=tuple(info['P_shape']))
+
+labels = np.unique(Y)
+num_labels = len(labels)
+
+Y = np.where(Y.reshape(-1,1)==labels)[1]  # to numeric
+
+print('X shape:', X.shape)
+print('Y shape:', Y.shape)
+print('T shape:', T.shape)
+print('P shape:', P.shape)
+
+# %% [markdown]
+'''
+## Train/test split
+'''
+
+# %%
+
+# Take out 10 participants
+test_ids = ['002', '003', '004', '005', '006', 
+            '007', '008', '009', '010', '011']
+mask_test = np.isin(P, test_ids)
+mask_train = ~mask_test
+X_train, Y_train, P_train, T_train = \
+    utils.ArrayFromMask(X, mask_train), Y[mask_train], P[mask_train], T[mask_train]
+X_test, Y_test, P_test, T_test = \
+    utils.ArrayFromMask(X, mask_test), Y[mask_test], P[mask_test], T[mask_test]
+print("Shape of X_train:", X_train.shape)
+print("Shape of X_test:", X_test.shape)
+
+# %% [markdown]
+'''
+## Architecture design
+
+As a baseline, let's use a convolutional neural network (CNN) with a
 typical pyramid-like structure. The input to the network is a `(N,3,3000)`
-array, corresponding to `N` instances of raw triaxial measurements of
-activity.
-The output of the network is a `(N,5)` array, each row containing predicted
-*unnormalized class scores*.
-To obtain probabilities, we can pass each row to a softmax. Then to report a
-class label, we can pick the highest probability in each row. We output class
-scores instead of class probabilities or labels because the loss function
-that we will use operates on the scores [(torch.CrossEntropyLoss)](https://pytorch.org/docs/stable/nn.html#crossentropyloss).
+array, corresponding to `N` windows of raw tri-axial accelerometer measures.
+Note the transposed format `(3,3000)` instead of `(3000,3)`; this *channels
+first* format is the default in PyTorch.
+
+The output of the CNN is a `(N,num_labels)` array where each row contains
+predicted unnormalized class scores or *logits*; pass each row to a softmax
+if you want to convert it to probabilities.
 '''
 
 # %%
@@ -130,15 +146,13 @@ class CNN(nn.Module):
     def forward(self, x):
         return self.cnn(x).view(x.shape[0],-1)
 
-# %%
+# %% [markdown]
 '''
-###### Helper functions
+## Helper functions
 
-We need a few helper functions:
-- A data loader that will provide the mini-batches during training.
-- A helper function that forward-passes the model on a dataset by chunks &mdash; this is simply to prevent the memory from blowing up.
-- A function to train a Hidden Markov Model (HMM) using the probabilistic predictions of the model for smoothing.
-- A function that evaluates the model (CNN + HMM) on a dataset, to be used to track the performance during training.
+- A data loader to provide the mini-batches during training.
+- A function to forward-pass the model on a dataset by chunks &mdash; this is simply to prevent the memory from blowing up.
+- A function to evaluate the model to track performance during training.
 '''
 
 # %%
@@ -152,7 +166,8 @@ def create_dataloader(X, y=None, batch_size=1, shuffle=False):
         idxs = np.arange(len(X))
     for i in range(0, len(idxs), batch_size):
         idxs_batch = idxs[i:i+batch_size]
-        X_batch = X[idxs_batch]
+        X_batch = X[idxs_batch].astype('f4')  # PyTorch defaults to float32
+        X_batch = np.transpose(X_batch, (0,2,1))  # channels first: (N,M,3) -> (N,3,M). PyTorch uses channel first format
         X_batch = torch.from_numpy(X_batch)
         if y is None:
             yield X_batch
@@ -163,8 +178,8 @@ def create_dataloader(X, y=None, batch_size=1, shuffle=False):
 
 
 def forward_by_batches(cnn, X):
-    ''' Forward pass model on a dataset. Do this by batches so that we do
-    not blow up the memory. '''
+    ''' Forward pass model on a dataset. 
+    Do this by batches so that we don't blow up the memory. '''
     Y = []
     cnn.eval()
     with torch.no_grad():
@@ -176,42 +191,32 @@ def forward_by_batches(cnn, X):
     return Y
 
 
-def train_hmm(cnn, X, y):
-    ''' Use the probabilistic predictions of the CNN to compute the Hidden
-    Markov Model parameters '''
+def evaluate_model(cnn, X, Y):
     Y_pred = forward_by_batches(cnn, X)  # scores
-    Y_pred = F.softmax(Y_pred, dim=1)  # probabilities
-    Y_pred = Y_pred.cpu().numpy()  # cast to numpy array
-    prior, emission, transition = utils.train_hmm(Y_pred, y)
-    return prior, emission, transition
+    loss = F.cross_entropy(Y_pred, torch.from_numpy(Y).to(device)).item()
 
-
-def evaluate_model(cnn, prior, emission, transition, X, y, pid=None):
-    Y_pred = forward_by_batches(cnn, X)  # scores
-    loss = F.cross_entropy(Y_pred, torch.from_numpy(y).to(device)).item()
     Y_pred = F.softmax(Y_pred, dim=1)  # convert to probabilities
-    y_pred = torch.argmax(Y_pred, dim=1)  # convert to classes
-    y_pred = y_pred.cpu().numpy()  # cast to numpy array
-    y_pred = utils.viterbi(y_pred, prior, emission, transition)  # HMM smoothing
-    scores = utils.compute_scores(y, y_pred)
-    return loss, scores
+    Y_pred = torch.argmax(Y_pred, dim=1)  # convert to classes
+    Y_pred = Y_pred.cpu().numpy()  # cast to numpy array
+    kappa = metrics.cohen_kappa_score(Y, Y_pred)
 
-# %%
+    return {'loss':loss, 'kappa':kappa, 'Y_pred':Y_pred}
+
+# %% [markdown]
 '''
-###### Hyperparameters, model instantiation, loss function and optimizer
+## Hyperparameters, model instantiation, loss function and optimizer
 
-Now we set the hyperparameters, instantiate the model, define the loss
-function (we use cross entropy for multiclass classification) and optimizer
-(we use AMSGRAD &mdash; a variant of SGD).
+Set the hyperparameters, instantiate the model, define the loss
+function (cross entropy for classification) and optimizer.
 '''
 
 # %%
 num_filters_init = 8  # initial num of filters -- see class definition
-in_channels = 3  # num channels of the signal -- equal to 3 for our raw triaxial timeseries
-output_size = 5  # number of classes (sleep, sedentary, etc...)
-num_epoch = 5  # num epochs (full loops though the training set) for SGD training
-lr = 1e-3  # learning rate in SGD
-batch_size = 32  # size of the mini-batch in SGD
+in_channels = 3  # num of channels of the signal -- equal to 3 for our raw triaxial timeseries
+output_size = num_labels  # num of classes (sleep, sedentary, etc...)
+num_epoch = 5  # num of epochs (full loops though the training set)
+lr = 1e-3  # learning rate
+batch_size = 32  # size of the mini-batch
 
 cnn = CNN(
     output_size=output_size,
@@ -223,21 +228,18 @@ print(cnn)
 loss_fn = nn.CrossEntropyLoss()
 optimizer = optim.Adam(cnn.parameters(), lr=lr, amsgrad=True)
 
-# %%
+# %% [markdown]
 '''
-###### Training
+## Training
 
-Training via mini-batch gradient descent begins here. We loop through the training set `num_epoch` times with the `dataloader` iterator.
 '''
 
 # %%
-accuracy_history = []
-balanced_accuracy_history = []
 kappa_history = []
 loss_history = []
 loss_history_train = []
 for i in tqdm(range(num_epoch)):
-    dataloader = create_dataloader(X_train, y_train, batch_size, shuffle=True)
+    dataloader = create_dataloader(X_train, Y_train, batch_size, shuffle=True)
     losses = []
     for x, target in dataloader:
         x, target = x.to(device), target.to(device)
@@ -250,82 +252,39 @@ for i in tqdm(range(num_epoch)):
         # Logging -- track train loss
         losses.append(loss.item())
 
-    # -------------------------------------------------------------------------
-    # Evaluate performance at the end of each epoch (full loop through the
-    # training set). We could also do this at every iteration, but this would
-    # be very expensive since we are evaluating on a large dataset.
-    # Aditionally, at the end of each epoch we train a Hidden Markov Model to
-    # smooth the predictions of the CNN.
-    # -------------------------------------------------------------------------
+    # --------------------------------------------------------
+    #       Evaluate performance at the end of each epoch 
+    # --------------------------------------------------------
 
     # Logging -- average train loss in this epoch
     loss_history_train.append(np.mean(losses))
 
-    # Compute HMM params
-    prior, emission, transition = train_hmm(cnn, X_train, y_train)
-
     # Logging -- evalutate performance on test set
-    loss_test, scores_test = evaluate_model(
-        cnn, prior, emission, transition, X_test, y_test, pid_test
-    )
-    loss_history.append(loss_test)
-    accuracy_history.append(scores_test['accuracy'])
-    balanced_accuracy_history.append(scores_test['balanced_accuracy'])
-    kappa_history.append(scores_test['kappa'])
+    results = evaluate_model(cnn, X_test, Y_test)
+    loss_history.append(results['loss'])
+    kappa_history.append(results['kappa'])
 
-# %%
-''' ###### Plot score and loss history '''
+# %% [markdown]
+''' ## Model performane '''
 
 # %%
 # Loss history
+plt.close('all')
 fig, ax = plt.subplots()
 ax.plot(loss_history_train, color='C0', label='train')
 ax.plot(loss_history, color='C1', label='test')
-ax.set_ylabel('loss')
+ax.set_ylabel('loss (CE)')
 ax.set_xlabel('epoch')
-ax.legend()
+ax = ax.twinx()
+ax.plot(kappa_history, color='C2', label='kappa')
+ax.set_ylabel('kappa')
+fig.legend()
 fig.show()
 
-# Scores history
-fig, ax = plt.subplots()
-ax.plot(accuracy_history, label='accuracy')
-ax.plot(balanced_accuracy_history, label='balanced accuracy')
-ax.plot(kappa_history, label='kappa')
-ax.set_ylabel('score')
-ax.set_xlabel('epoch')
-ax.legend()
-fig.show()
-
-# Scores details -- last epoch
-utils.print_scores(scores_test)
+# Report
+Y_test_pred_lab = labels[results['Y_pred']]  # to labels
+Y_test_lab = labels[Y_test]  # to labels
+print('\nClassifier performance')
+print('Out of sample:\n', metrics.classification_report(Y_test_lab, Y_test_pred_lab)) 
 
 # %%
-'''
-###### Ideas
-- Implement early stopping to select the model at its best out-of-sample performance. Do we track kappa or accuracy?
-- Helping the model to learn:
-    - As an attempt to remove gravity, try detrending the signal. You can implement this in the dataloader:
-
-        ```python
-        # Do this before torch.from_numpy()
-        X_batch = scipy.signal.detrend(X_batch, type='constant')
-        ```
-
-        See [scipy.signal.detrend](https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.detrend.html). More generally, consider removing the low frequencies of the signal, e.g. 0 - 0.5Hz.
-
-    - Add a 4th channel containing the vector magnitude, i.e. $(x,y,z,r)$ where $r = \sqrt{x^2+y^2+z^2}$. You can implement this in the dataloader:
-
-        ```python
-        # Do this before torch.from_numpy()
-        X_batch = np.concatenate((X_batch, np.linalg.norm(X_batch, axis=1, keepdims=True)), axis=1)
-        ```
-
-    - Consider using [spherical coordinates](https://en.wikipedia.org/wiki/Spherical_coordinate_system), or adding them as additional channels.
-
-- Modify the architecture. Feel free to use recent architectures such as ResNet.
-
-###### References
-
-- [A recipe for training neural networks](http://karpathy.github.io/2019/04/25/recipe/)
-- [PyTorch's Conv1d module](https://pytorch.org/docs/stable/nn.html#conv1d)
-'''
