3c2844dc193004d5521e56215e182de44c3a119f
Shing Chan <cshing.m@gmail.com>
2021-03-14 22:39:10 +0000

    cohort2020 update

--- utils.py
+++ utils.py
@@ -1,103 +1,35 @@
 import numpy as np
-import sklearn.metrics as metrics
+import pandas as pd
+import scipy.stats as stats
 
 
-# class code (tip to remember: goes from lower to higher activity)
-CLASS_CODE = {'sleep': 0, 'sedentary': 1, 'tasks-light': 2, 'walking': 3, 'moderate': 4}
-# list of classes, ordered by code
-CLASSES = ['sleep', 'sedentary', 'tasks-light', 'walking', 'moderate']
-NUM_CLASSES = len(CLASSES)
-# colors to be used for each class, ordered by code
-COLORS = ['blue', 'red', 'darkorange', 'lightgreen', 'green']
+def extract_features(xyz):
+    ''' Extract timeseries features. xyz is a window of shape (N,3) '''
+    
+    feats = {}
+    feats['xMean'], feats['yMean'], feats['zMean'] = np.mean(xyz, axis=0)
+    feats['xStd'], feats['yStd'], feats['zStd'] = np.std(xyz, axis=0)
+    feats['xRange'], feats['yRange'], feats['zRange'] = np.ptp(xyz, axis=0)
+    feats['xIQR'], feats['yIQR'], feats['zIQR'] = stats.iqr(xyz, axis=0)
 
-NUM_FEATS = 125  # number of hand-crafted features
-SAMPLE_RATE = 100  # device sample rate (100hz)
-RAW_SHAPE = (3,3000)  # triaxial x 30secs x 100hz
-RAW_DTYPE = 'float32'  # measurements are in float32 precision
+    x, y, z = xyz.T
 
+    with np.errstate(divide='ignore', invalid='ignore'):  # ignore div by 0 warnings
+        feats['xyCorr'] = np.nan_to_num(np.corrcoef(x, y)[0,1])
+        feats['yzCorr'] = np.nan_to_num(np.corrcoef(y, z)[0,1])
+        feats['zxCorr'] = np.nan_to_num(np.corrcoef(z, x)[0,1])
 
-def encode_one_hot(y):
-    '''
-    0 -> 1,0,0,0,0
-    1 -> 0,1,0,0,0
-    2 -> 0,0,1,0,0
-    3 -> 0,0,0,1,0
-    4 -> 0,0,0,0,1
-    '''
-    return (y.reshape(-1,1) == np.arange(NUM_CLASSES)).astype(int)
+    m = np.linalg.norm(xyz, axis=1)
 
+    feats['mean'] = np.mean(m)
+    feats['std'] = np.std(m)
+    feats['range'] = np.ptp(m)
+    feats['iqr'] = stats.iqr(m)
+    feats['mad'] = stats.median_abs_deviation(m)
+    feats['kurt'] = stats.kurtosis(m)
+    feats['skew'] = stats.skew(m)
 
-def train_hmm(Y_pred, y_true):
-    ''' https://en.wikipedia.org/wiki/Hidden_Markov_model '''
-    if Y_pred.ndim == 1 or Y_pred.shape[1] == 1:
-        Y_pred = encode_one_hot(Y_pred)
-
-    prior = np.mean(y_true.reshape(-1,1) == np.arange(NUM_CLASSES), axis=0)
-    emission = np.vstack(
-        [np.mean(Y_pred[y_true==i], axis=0) for i in range(NUM_CLASSES)]
-    )
-    transition = np.vstack(
-        [np.mean(y_true[1:][(y_true==i)[:-1]].reshape(-1,1) == np.arange(NUM_CLASSES), axis=0)
-            for i in range(NUM_CLASSES)]
-    )
-    return prior, emission, transition
-
-
-def viterbi(y_pred, prior, emission, transition):
-    ''' https://en.wikipedia.org/wiki/Viterbi_algorithm '''
-    small_number = 1e-16
-
-    def log(x):
-        return np.log(x + small_number)
-
-    num_obs = len(y_pred)
-    probs = np.zeros((num_obs, NUM_CLASSES))
-    probs[0,:] = log(prior) + log(emission[:, y_pred[0]])
-    for j in range(1, num_obs):
-        for i in range(NUM_CLASSES):
-            probs[j,i] = np.max(
-                log(emission[i, y_pred[j]]) + \
-                log(transition[:, i]) + \
-                probs[j-1,:])  # probs already in log scale
-    viterbi_path = np.zeros_like(y_pred)
-    viterbi_path[-1] = np.argmax(probs[-1,:])
-    for j in reversed(range(num_obs-1)):
-        viterbi_path[j] = np.argmax(
-            log(transition[:, viterbi_path[j+1]]) + \
-            probs[j,:])  # probs already in log scale
-
-    return viterbi_path
-
-
-def compute_scores(y_true, y_pred):
-    ''' Compute a bunch of scoring functions '''
-    confusion = metrics.confusion_matrix(y_true, y_pred)
-    per_class_recall = metrics.recall_score(y_true, y_pred, average=None)
-    accuracy = metrics.accuracy_score(y_true, y_pred)
-    balanced_acuracy = metrics.balanced_accuracy_score(y_true, y_pred)
-    kappa = metrics.cohen_kappa_score(y_true, y_pred)
-    return {
-        'confusion':confusion,
-        'per_class_recall':per_class_recall,
-        'accuracy': accuracy,
-        'balanced_accuracy': balanced_acuracy,
-        'kappa':kappa,
-    }
-
-
-def print_scores(scores):
-    print("Accuracy score:", scores['accuracy'])
-    print("Balanced accuracy score:", scores['balanced_accuracy'])
-    print("Cohen kappa score:", scores['kappa'])
-    print("\nPer-class recall scores:")
-    print(
-        "sleep      : {}\n"
-        "sedentary  : {}\n"
-        "tasks-light: {}\n"
-        "walking    : {}\n"
-        "moderate   : {}".format(*scores['per_class_recall'])
-    )
-    print("\nConfusion matrix:\n", scores['confusion'])
+    return feats
 
 
 class ArrayFromIdxs(object):
@@ -124,37 +56,9 @@ class ArrayFromMask(ArrayFromIdxs):
         super().__init__(X, idxs)
 
 
-# -------------------------------------------
-#  Code for hand-crafted feature extraction
-# -------------------------------------------
-import jpype
-import jpype.imports
-class Extractor():
-    ''' A wrapper of the Java class FeatureExtractor using the JPype package.
-    It starts a Java Virtual Machine, instantiates FeatureExtractor, and
-    implements 'extract' method to handle numpy arrays.
-    '''
-    def __init__(self):
-        # start Java Virtual Machine and instantiate FeatureExtractor
-        if not jpype.isJVMStarted():
-            jpype.addClassPath("java_feature_extractor")
-            jpype.addClassPath("java_feature_extractor/JTransforms-3.1-with-dependencies.jar")
-            jpype.startJVM(convertStrings=False)
-        self.java_extractor = jpype.JClass('FeatureExtractor')
-
-    def extract(self, xyz):
-        xArray, yArray, zArray = xyz
-        xArray = jpype.JArray(jpype.JFloat, 1)(xArray)
-        yArray = jpype.JArray(jpype.JFloat, 1)(yArray)
-        zArray = jpype.JArray(jpype.JFloat, 1)(zArray)
-        return np.asarray(self.java_extractor.extract(
-            xArray, yArray, zArray, SAMPLE_RATE))
-
-
-# ----------------------------------------
-#  Function for activity timeseries plot
-# ----------------------------------------
-import pandas as pd
+# -----------------------------------
+#       plot_compare_activity()
+# -----------------------------------
 import matplotlib.pyplot as plt
 import matplotlib
 import matplotlib.lines as mlines
@@ -162,76 +66,127 @@ import matplotlib.patches as mpatches
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 from datetime import datetime, timedelta, time
-def plot_activity(x, y, t):
-    ''' Plot activity timeseries '''
-    BACKGROUND_COLOR = '#d3d3d3' # lightgray
 
-    def split_by_timegap(group, seconds=30):
-        subgroupIDs = (group.index.to_series().diff() > timedelta(seconds=seconds)).cumsum()
-        subgroups = group.groupby(by=subgroupIDs)
-        return subgroups
+LABEL_COLOURS = {'sleep':'blue', 
+                'sit-stand': 'red',
+                'vehicle': 'darkorange',
+                'walking': 'lightgreen',
+                'mixed': 'lime',
+                'bicycling': 'green',
+                'tasks-light': 'darkorange',
+                'moderate': 'green',
+                'sedentary': 'red',
+                'light': 'darkorange',
+                'moderage-vigorous': 'green',}
+
+BACKGROUND_COLOR = '#d3d3d3' # lightgray
+
+
+def plot_compare_activity(t, y_true, y_pred, accel=None):
+    ''' Plot and compare activity profiles '''
 
     convert_date = np.vectorize(
         lambda day, x: matplotlib.dates.date2num(datetime.combine(day, x)))
-    timeseries = pd.DataFrame(data={'x':x, 'y':y, 't':t})
-    timeseries.set_index('t', inplace=True)
-    timeseries['x'] = timeseries['x'].rolling(window=12, min_periods=1).mean()  #! inplace?
-    ylim_min, ylim_max = np.min(x), np.max(x)
-    groups = timeseries.groupby(timeseries.index.date)
-    fig, axs = plt.subplots(nrows=len(groups) + 1)
-    for ax, (day, group) in zip(axs, groups):
-        for _, subgroup in split_by_timegap(group):
-            _t = convert_date(day, subgroup.index.time)
-            _ys = [(subgroup['y'] == i).astype('int') * ylim_max for i in range(NUM_CLASSES)]
-            ax.plot(_t, subgroup['x'], c='k')
-            ax.stackplot(_t, _ys, colors=COLORS, alpha=.5, edgecolor='none')
 
-        ax.get_xaxis().grid(True, which='major', color='grey', alpha=0.5)
-        ax.get_xaxis().grid(True, which='minor', color='grey', alpha=0.25)
-        ax.set_xlim((datetime.combine(day,time(0, 0, 0, 0)),
-            datetime.combine(day + timedelta(days=1), time(0, 0, 0, 0))))
-        ax.set_xticks(pd.date_range(start=datetime.combine(day,time(0, 0, 0, 0)),
-            end=datetime.combine(day + timedelta(days=1), time(0, 0, 0, 0)),
-            freq='4H'))
-        ax.set_xticks(pd.date_range(start=datetime.combine(day,time(0, 0, 0, 0)),
-            end=datetime.combine(day + timedelta(days=1), time(0, 0, 0, 0)),
-            freq='1H'), minor=True)
-        ax.set_ylim((ylim_min, ylim_max))
-        ax.get_yaxis().set_ticks([]) # hide y-axis lables
-        ax.spines['top'].set_color(BACKGROUND_COLOR)
-        ax.spines['right'].set_visible(False)
-        ax.spines['left'].set_visible(False)
-        ax.set_facecolor(BACKGROUND_COLOR)
+    data = pd.DataFrame(data={'time':t, 
+                              'y_true': y_true, 
+                              'y_pred': y_pred, 
+                              'accel': accel})
+    data.set_index('time', inplace=True)
 
-        ax.set_title(
+    # Preprocess accel so that plot is nicer by median-smoothing and rescaling to [0,1] 
+    data['accel'] = data['accel'].rolling(window=10, min_periods=1).median()
+    data['accel'] = (data['accel'] - 1.0).abs()  # "movement" (because exact 1g is stationary)
+    data['accel'] = data['accel'] / (data['accel'].max() - data['accel'].min())  # scale to [0,1]
+
+    labels = np.unique(y_true)
+    colors = [LABEL_COLOURS[label] for label in labels]
+    data_by_days = data.groupby(data.index.date)
+    ndays = len(data_by_days)
+    nrows = 3*ndays + 1  # ndays x (prediction + annotation + spacing) + legend
+
+    fig = plt.figure(figsize=(10,nrows), dpi=200)
+    gs = fig.add_gridspec(nrows=nrows, ncols=1, height_ratios=[2, 2, 1]*ndays+[2])
+    axes = [fig.add_subplot(gs[i]) for i in range(nrows) if (i+1)%3]  # every 3rd axes is a spacing
+
+    for i, (day, daydata) in enumerate(data_by_days):
+
+        ax_true, ax_pred = axes[2*i], axes[2*i+1]
+
+        for _, chunk in splitby_timegap(daydata):
+            _t = convert_date(day, chunk.index.time)
+            _y_true_dummies = [(chunk['y_true']==label).astype('int') for label in labels]
+            _y_pred_dummies = [(chunk['y_pred']==label).astype('int') for label in labels]
+            ax_true.plot(_t, chunk['accel'], c='k')
+            ax_pred.plot(_t, chunk['accel'], c='k')
+            ax_true.stackplot(_t, _y_true_dummies, colors=colors, alpha=.5, edgecolor='none')
+            ax_pred.stackplot(_t, _y_pred_dummies, colors=colors, alpha=.5, edgecolor='none')
+
+            format_axes(ax_true, day)
+            format_axes(ax_pred, day)
+
+        ax_true.set_ylabel('true', fontsize='x-small')
+        ax_pred.set_ylabel('predicted', fontsize='x-small')
+
+        # add date to left side of axes
+        ax_pred.set_title(
             day.strftime("%A,\n%d %B"), weight='bold',
-            x=-.2, y=.5,
+            x=-.2, y=-.3,
             horizontalalignment='left',
             verticalalignment='bottom',
             rotation='horizontal',
-            transform=ax.transAxes,
+            transform=ax_pred.transAxes,
             fontsize='medium',
             color='k'
         )
 
     # legends
-    axs[-1].axis('off')
+    axes[-1].axis('off')
     legend_patches = []
     legend_patches.append(mlines.Line2D([], [], color='k', label='acceleration'))
-    for color, label in zip(COLORS, CLASSES):
-        legend_patches.append(mpatches.Patch(facecolor=color, label=label, alpha=0.5))
-    axs[-1].legend(handles=legend_patches, bbox_to_anchor=(0., 0., 1., 1.),
-        loc='center', ncol=min(3,len(legend_patches)), mode="best",
+    for label, color in zip(labels, colors):
+        legend_patches.append(mpatches.Patch(facecolor=color, label=label, alpha=.5))
+    axes[-1].legend(handles=legend_patches, bbox_to_anchor=(0., 0., 1., 1.),
+        # loc='center', ncol=min(3,len(legend_patches)), mode="best",
+        loc='center', ncol=len(legend_patches), mode="best",
         borderaxespad=0, framealpha=0.6, frameon=True, fancybox=True)
-    axs[-1].spines['left'].set_visible(False)
-    axs[-1].spines['right'].set_visible(False)
-    axs[-1].spines['top'].set_visible(False)
-    axs[-1].spines['bottom'].set_visible(False)
+    axes[-1].spines['left'].set_visible(False)
+    axes[-1].spines['right'].set_visible(False)
+    axes[-1].spines['top'].set_visible(False)
+    axes[-1].spines['bottom'].set_visible(False)
 
     # format x-axis to show hours
     fig.autofmt_xdate()
-    hours = [(str(hr) + 'am') if hr<=12 else (str(hr-12) + 'pm') for hr in range(0,24,4)]
-    axs[0].set_xticklabels(hours)
-    axs[0].tick_params(labelbottom=False, labeltop=True, labelleft=False)
+    hours = ['00:00', '04:00', '08:00', '12:00', '16:00', '20:00', '24:00']
+    axes[0].set_xticklabels(hours)
+    axes[0].tick_params(labelbottom=False, labeltop=True, labelleft=False)
 
-    return fig, axs
+    fig.show()
+
+
+def format_axes(ax, day):
+    # run gridlines for each hour bar
+    ax.get_xaxis().grid(True, which='major', color='grey', alpha=0.5)
+    ax.get_xaxis().grid(True, which='minor', color='grey', alpha=0.25)
+    # set x and y-axes
+    ax.set_xlim((datetime.combine(day,time(0, 0, 0, 0)),
+        datetime.combine(day + timedelta(days=1), time(0, 0, 0, 0))))
+    ax.set_xticks(pd.date_range(start=datetime.combine(day,time(0, 0, 0, 0)),
+        end=datetime.combine(day + timedelta(days=1), time(0, 0, 0, 0)),
+        freq='4H'))
+    ax.set_xticks(pd.date_range(start=datetime.combine(day,time(0, 0, 0, 0)),
+        end=datetime.combine(day + timedelta(days=1), time(0, 0, 0, 0)),
+        freq='1H'), minor=True)
+    ax.get_yaxis().set_ticks([]) # hide y-axis lables
+    # make border less harsh between subplots
+    ax.spines['top'].set_color(BACKGROUND_COLOR)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    # set background colour to lightgray
+    ax.set_facecolor(BACKGROUND_COLOR)
+
+
+def splitby_timegap(data, gap=30):
+    split_id = (data.index.to_series().diff() > pd.Timedelta(gap, 'S')).cumsum()
+    splits = data.groupby(by=split_id)
+    return splits
