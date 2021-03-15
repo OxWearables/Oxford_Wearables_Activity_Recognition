62742365024b787d8a124c12ffbc2bfecbd3a640
Shing Chan <cshing.m@gmail.com>
2021-03-15 00:20:13 +0000

    cohort2020 update

--- src/spectrogram.py
+++ src/spectrogram.py
@@ -1,10 +1,6 @@
-# %%
+# %% [markdown]
 '''
-# Activity recognition on the Capture24 dataset
-
-## Training on the spectrogram of the signal
-
-*This section assumes familiarity with [PyTorch](https://pytorch.org/)*
+# Training on the spectrogram of the signal
 
 Computing the [spectrogram](https://en.wikipedia.org/wiki/Spectrogram) of a
 signal is a common visualization method in signal processing to treat
@@ -12,10 +8,11 @@ signals as 2D images. Recent works have looked at using the spectrogram
 for classification, thus converting the signal recognition task into an image
 recognition one and leveraging techniques from computer vision.
 
-###### Setup
+## Setup
 '''
 
 # %%
+import json
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal
@@ -25,6 +22,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 import torch.backends.cudnn as cudnn
 from tqdm.auto import tqdm
+from sklearn import metrics
 import utils
 
 # For reproducibility
@@ -32,10 +30,7 @@ np.random.seed(42)
 torch.manual_seed(42)
 cudnn.benchmark = True
 
-# %%
-''' ###### Grab a GPU if there is one '''
-
-# %%
+# Grab a GPU if there is one
 if torch.cuda.is_available():
     device = torch.device("cuda")
     print("Using {} device: {}".format(device, torch.cuda.current_device()))
@@ -43,32 +38,55 @@ else:
     device = torch.device("cpu")
     print("Using {}".format(device))
 
-# %%
-''' ###### Load dataset and hold out some instances for testing '''
+# %% [markdown]
+''' 
+## Load dataset 
+'''
 
 # %%
-data = np.load('capture24.npz', allow_pickle=True)
-# data = np.load('capture24_small.npz', allow_pickle=True)
-print("Contents of capture24.npz:", data.files)
-y, pid, time = data['y'], data['pid'], data['time']
-X = np.load('X_raw.npy', mmap_mode='r')
-# X = np.load('X_raw_small.npy')
-print("Raw dataset shape:", X.shape)
 
-# Hold out some participants for testing the model
-pids_test = [2, 3]
-mask_test = np.isin(pid, pids_test)
+DATASET_PATH = 'dataset/'  # path to your extracted windows
+
+with open(DATASET_PATH+'info.json', 'r') as f:
+    info = json.load(f)
+
+X = np.memmap(DATASET_PATH+'X.dat', mode='r', dtype=info['X_dtype'], shape=tuple(info['X_shape']))
+Y = np.memmap(DATASET_PATH+'Y.dat', mode='r', dtype=info['Y_dtype'], shape=tuple(info['Y_shape']))
+T = np.memmap(DATASET_PATH+'T.dat', mode='r', dtype=info['T_dtype'], shape=tuple(info['T_shape']))
+P = np.memmap(DATASET_PATH+'P.dat', mode='r', dtype=info['P_dtype'], shape=tuple(info['P_shape']))
+
+labels = np.unique(Y)
+num_labels = len(labels)
+
+Y = np.where(Y.reshape(-1,1)==labels)[1]  # to numeric
+
+print('X shape:', X.shape)
+print('Y shape:', Y.shape)
+print('T shape:', T.shape)
+print('P shape:', P.shape)
+
+# %% [markdown]
+'''
+## Train/test split
+'''
+
+# %%
+
+# Take out 10 participants
+test_ids = ['002', '003', '004', '005', '006', 
+            '007', '008', '009', '010', '011']
+mask_test = np.isin(P, test_ids)
 mask_train = ~mask_test
-y_train, y_test = y[mask_train], y[mask_test]
-pid_train, pid_test = pid[mask_train], pid[mask_test]
-# X[mask_train] and X[mask_test] if you like to live dangerously
-X_train = utils.ArrayFromMask(X, mask_train)
-X_test = utils.ArrayFromMask(X, mask_test)
-print("Shape of X_train", X_train.shape)
-print("Shape of X_test", X_test.shape)
+X_train, Y_train, P_train, T_train = \
+    utils.ArrayFromMask(X, mask_train), Y[mask_train], P[mask_train], T[mask_train]
+X_test, Y_test, P_test, T_test = \
+    utils.ArrayFromMask(X, mask_test), Y[mask_test], P[mask_test], T[mask_test]
+print("Shape of X_train:", X_train.shape)
+print("Shape of X_test:", X_test.shape)
 
-# %%
-''' ###### Visualization
+
+# %% [markdown]
+''' ## Visualization
 
 Let's visualize the spectrograms of the acceleration norm for each activity class.
 We use
@@ -86,13 +104,13 @@ HOP_LENGTH = 48
 WINDOW = 'hann'
 
 NUM_PLOTS = 10
-fig, axs = plt.subplots(utils.NUM_CLASSES, NUM_PLOTS, figsize=(10,5))
-for i in range(utils.NUM_CLASSES):
-    axs[i,0].set_ylabel(utils.CLASSES[i])
-    idxs = np.where(y==i)[0]
+fig, axs = plt.subplots(num_labels, NUM_PLOTS, figsize=(10,5))
+for i in range(num_labels):
+    axs[i,0].set_ylabel(labels[i])
+    idxs = np.where(Y==i)[0]
     for j in range(NUM_PLOTS):
         _, _, z = scipy.signal.stft(
-            np.linalg.norm(X[idxs[j]], axis=0),  # acceleration vector norm
+            np.linalg.norm(X[idxs[j]], axis=1),  # acceleration vector norm
             nperseg=N_FFT,
             noverlap=N_FFT-HOP_LENGTH,
             window=WINDOW,
@@ -104,27 +122,19 @@ for i in range(utils.NUM_CLASSES):
         axs[i,j].set_yticks([])
 fig.show()
 
-# %%
-''' ## Classifying spectrograms for activity recognition
-
-The rest of this notebook is identical to the previous notebook on neural
-networks except for the model architecture which now uses 2D convolutions and
-expects images, and the data loader that generates the spectrograms.
+# %% [markdown]
 '''
+## Architecture design
 
-# %%
-'''
-###### Architecture design
-
-As a baseline model, we use a convolutional neural network (CNN) with a
+As a baseline, let's use a convolutional neural network (CNN) with a
 typical pyramid-like structure. The input to the network is a `(N,3,61,61)`
-array, corresponding to `N` spectrograms of the accelerometer signals.
-The output of the network is a `(N,5)` array, each row containing predicted
-*unnormalized class scores*.
-To obtain probabilities, we can pass each row to a softmax. Then to report a
-class label, we can pick the highest probability in each row. We output class
-scores instead of class probabilities or labels because the loss function
-that we will use operates on the scores [(torch.CrossEntropyLoss)](https://pytorch.org/docs/stable/nn.html#crossentropyloss).
+array, corresponding to `N` spectrograms for each axis signal. Again, not the
+*channels-first* format: `(3,61,61)` instead of `(61,61,3)`.
+
+The output of the CNN is a `(N,num_labels)` array where each row contains
+predicted unnormalized class scores or *logits*; pass each row to a softmax
+if you want to convert it to probabilities.
+
 '''
 
 # %%
@@ -169,15 +179,13 @@ class CNN(nn.Module):
     def forward(self, x):
         return self.cnn(x).view(x.shape[0],-1)
 
-# %%
+# %% [markdown]
 '''
-###### Helper functions
+## Helper functions
 
-We need a few helper functions:
-- A data loader that will provide the mini-batches during training. Here we also generate the batch of spectrograms. While one could do this using [scipy.signal.stft](https://docs.scipy.org/doc/scipy-0.19.1/reference/generated/scipy.signal.stft.html), here we use PyTorch's [torch.stft](https://pytorch.org/docs/stable/torch.html#torch.stft) that does the same thing. The benefit is that we can then allow the computation to be done in the GPU if it is available.
-- A helper function that forward-passes the model on a dataset by chunks &mdash; this is simply to prevent the memory from blowing up.
-- A function to train a Hidden Markov Model (HMM) using the probabilistic predictions of the model for smoothing.
-- A function that evaluates the model (CNN + HMM) on a dataset, to be used to track the performance during training.
+- A data loader to provide the mini-batches during training.
+- A function to forward-pass the model on a dataset by chunks &mdash; this is simply to prevent the memory from blowing up.
+- A function to evaluate the model to track performance during training.
 '''
 
 # %%
@@ -197,16 +205,19 @@ def create_dataloader(X, y=None, batch_size=1, shuffle=False):
         idxs = np.arange(len(X))
     for i in range(0, len(idxs), batch_size):
         idxs_batch = idxs[i:i+batch_size]
-        X_batch = torch.from_numpy(X[idxs_batch])
+        X_batch = X[idxs_batch].astype('f4')  # PyTorch defaults to float32
+        X_batch = np.transpose(X_batch, (0,2,1))  # channels first: (N,M,3) -> (N,3,M)
+        X_batch = torch.from_numpy(X_batch)
         Z_batch = torch.stft(
             # Pack channel and batch dimensions. Also upload the batch to GPU
             # (if available) to compute the STFT.
             # Let the GPU do the work -- Gordon Ramsey
-            X_batch.view(-1, X_batch.shape[-1]).to(device),
+            X_batch.reshape(-1, X_batch.shape[-1]).to(device),
             n_fft=N_FFT,
             hop_length=HOP_LENGTH,
             window=WINDOW.to(device),
             center=False,
+            return_complex=False
         )
         # Unpack channel and batch dimensions
         Z_batch = Z_batch.view(*X_batch.shape[:2], *Z_batch.shape[1:])
@@ -220,8 +231,8 @@ def create_dataloader(X, y=None, batch_size=1, shuffle=False):
 
 
 def forward_by_batches(cnn, X):
-    ''' Forward pass model on a dataset. Do this by batches so that we do
-    not blow up the memory. '''
+    ''' Forward pass model on a dataset. 
+    Do this by batches so that we don't blow up the memory. '''
     Y = []
     cnn.eval()
     with torch.no_grad():
@@ -233,39 +244,27 @@ def forward_by_batches(cnn, X):
     return Y
 
 
-def train_hmm(cnn, X, y):
-    ''' Use the probabilistic predictions of the CNN to compute the Hidden
-    Markov Model parameters '''
+def evaluate_model(cnn, X, Y):
     Y_pred = forward_by_batches(cnn, X)  # scores
-    Y_pred = F.softmax(Y_pred, dim=1)  # probabilities
-    Y_pred = Y_pred.cpu().numpy()  # cast to numpy array
-    prior, emission, transition = utils.train_hmm(Y_pred, y)
-    return prior, emission, transition
+    loss = F.cross_entropy(Y_pred, torch.from_numpy(Y).to(device)).item()
 
-
-def evaluate_model(cnn, prior, emission, transition, X, y, pid=None):
-    Y_pred = forward_by_batches(cnn, X)  # scores
-    loss = F.cross_entropy(Y_pred, torch.from_numpy(y).to(device)).item()
     Y_pred = F.softmax(Y_pred, dim=1)  # convert to probabilities
-    y_pred = torch.argmax(Y_pred, dim=1)  # convert to classes
-    y_pred = y_pred.cpu().numpy()  # cast to numpy array
-    y_pred = utils.viterbi(y_pred, prior, emission, transition)  # HMM smoothing
-    scores = utils.compute_scores(y, y_pred)
-    return loss, scores
+    Y_pred = torch.argmax(Y_pred, dim=1)  # convert to classes
+    Y_pred = Y_pred.cpu().numpy()  # cast to numpy array
+    kappa = metrics.cohen_kappa_score(Y, Y_pred)
 
-# %%
+    return {'loss':loss, 'kappa':kappa, 'Y_pred':Y_pred}
+
+# %% [markdown]
 '''
-###### Hyperparameters, model instantiation, loss function and optimizer
+## Hyperparameters, model instantiation, loss function and optimizer
 
-Now we set the hyperparameters, instantiate the model, define the loss
-function (we use cross entropy for multiclass classification) and optimizer
-(we use AMSGRAD &mdash; a variant of SGD).
 '''
 
 # %%
 num_filters_init = 32  # initial num of filters -- see class definition
 in_channels = 3  # num channels of the signal -- equal to 3 for our raw triaxial timeseries
-output_size = 5  # number of classes (sleep, sedentary, etc...)
+output_size = num_labels  # number of classes (sleep, sedentary, etc...)
 num_epoch = 5  # num of epochs (full loops though the training set) for SGD training
 lr = 1e-3  # learning rate in SGD
 batch_size = 32  # size of the mini-batch in SGD
@@ -280,21 +279,18 @@ print(cnn)
 loss_fn = nn.CrossEntropyLoss()
 optimizer = optim.Adam(cnn.parameters(), lr=lr, amsgrad=True)
 
-# %%
+# %% [markdown]
 '''
-###### Training
+## Training
 
-Training via mini-batch gradient descent begins here. We loop through the training set `num_epoch` times with the `dataloader` iterator.
 '''
 
 # %%
-accuracy_history = []
-balanced_accuracy_history = []
 kappa_history = []
 loss_history = []
 loss_history_train = []
 for i in tqdm(range(num_epoch)):
-    dataloader = create_dataloader(X_train, y_train, batch_size, shuffle=True)
+    dataloader = create_dataloader(X_train, Y_train, batch_size, shuffle=True)
     losses = []
     for z, target in dataloader:
         z, target = z.to(device), target.to(device)
@@ -318,43 +314,34 @@ for i in tqdm(range(num_epoch)):
     # Logging -- average train loss in this epoch
     loss_history_train.append(np.mean(losses))
 
-    # Compute HMM params
-    prior, emission, transition = train_hmm(cnn, X_train, y_train)
-
     # Logging -- evalutate performance on test set
-    loss_test, scores_test = evaluate_model(
-        cnn, prior, emission, transition, X_test, y_test, pid_test
-    )
-    loss_history.append(loss_test)
-    accuracy_history.append(scores_test['accuracy'])
-    balanced_accuracy_history.append(scores_test['balanced_accuracy'])
-    kappa_history.append(scores_test['kappa'])
+    results = evaluate_model(cnn, X_test, Y_test)
+    loss_history.append(results['loss'])
+    kappa_history.append(results['kappa'])
 
-# %%
-''' ###### Plot score and loss history '''
+# %% [markdown]
+''' ## Model performane '''
 
 # %%
 # Loss history
+plt.close('all')
 fig, ax = plt.subplots()
 ax.plot(loss_history_train, color='C0', label='train')
 ax.plot(loss_history, color='C1', label='test')
-ax.set_ylabel('loss')
+ax.set_ylabel('loss (CE)')
 ax.set_xlabel('epoch')
-ax.legend()
+ax = ax.twinx()
+ax.plot(kappa_history, color='C2', label='kappa')
+ax.set_ylabel('kappa')
+fig.legend()
 fig.show()
 
-# Scores history
-fig, ax = plt.subplots()
-ax.plot(accuracy_history, label='accuracy')
-ax.plot(balanced_accuracy_history, label='balanced accuracy')
-ax.plot(kappa_history, label='kappa')
-ax.set_ylabel('score')
-ax.set_xlabel('epoch')
-ax.legend()
-fig.show()
+# Report
+Y_test_pred_lab = labels[results['Y_pred']]  # to labels
+Y_test_lab = labels[Y_test]  # to labels
+print('\nClassifier performance')
+print('Out of sample:\n', metrics.classification_report(Y_test_lab, Y_test_pred_lab)) 
 
-# Scores details -- last epoch
-utils.print_scores(scores_test)
 
 # %%
 '''
