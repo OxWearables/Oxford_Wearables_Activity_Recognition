b7e9930c19015a509963590cb697f8d906ed5248
Shing Chan <cshing.m@gmail.com>
2024-09-25 09:09:38 +0000

    fix utils, my_device -> device, batch size, change to willetts labels

--- 6_self_supervised_learning/utils/utils.py
+++ 6_self_supervised_learning/utils/utils.py
@@ -35,7 +35,7 @@ class EarlyStopping:
         self.counter = 0
         self.best_score = None
         self.early_stop = False
-        self.val_loss_min = np.Inf
+        self.val_loss_min = np.inf
         self.delta = delta
         self.path = path
         self.trace_func = trace_func
